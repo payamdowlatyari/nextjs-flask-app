10f295ccd256032c4b89a3416b2851592f08069f
Payam Dowlatyari <pdowlatyari@gmail.com>
2025-07-30 09:06:21 -0700

    Added Vercel config for deploying

--- api/index.py
+++ api/index.py
@@ -254,6 +254,7 @@ def delete_note(note_id):
         return jsonify({'result': 'Deleted'})
     return jsonify({'error': 'Note not found'}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def handler(environ, start_response):
+    return app(environ, start_response)
+
 
